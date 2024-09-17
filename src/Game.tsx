c9c878e59e2b2e9c478121e46d67eb816dae4fd9
Martin Černý <mcmartincerny@gmail.com>
2024-09-18 01:22:12 +0200

    Grass and bricks

--- src/Game.tsx
+++ src/Game.tsx
@@ -3,12 +3,19 @@ import "./Game.css";
 import {
   DataTexture,
   Mesh,
+  MeshBasicMaterial,
+  MeshNormalMaterial,
   MeshPhongMaterial,
+  MeshStandardMaterial,
   MeshToonMaterial,
   PerspectiveCamera,
   PlaneGeometry,
   PointLight,
   RedFormat,
+  RGBAIntegerFormat,
+  RGBFormat,
+  RGBIntegerFormat,
+  RGFormat,
   Scene,
   SphereGeometry,
   WebGLRenderer,
@@ -25,27 +32,45 @@ const init = () => {
   console.log("init");
   const canvas = document.querySelector("#gameCanvas") as HTMLCanvasElement;
   const renderer = new WebGLRenderer({ antialias: true, canvas });
-  const camera = new PerspectiveCamera(75, 2, 0.1, 10);
-  camera.position.z = 2;
+  const camera = new PerspectiveCamera(75, 2, 0.1, 100);
   const controls = new OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
   controls.dampingFactor = 0.25;
+  camera.position.z = 4;
+  camera.position.y = -5;
+  controls.target.set(0, 3, 0);
 
   const scene = new Scene();
 
   const geometry = new SphereGeometry(1, 100, 100);
   const material = new MeshToonMaterial({ color: 0x44aa88, gradientMap: generateGradientMap() });
   const sphere = new Mesh(geometry, material);
-  sphere.rotation.x = 0.5;
+  sphere.position.z = 1;
+  sphere.position.y = 2;
   scene.add(sphere);
 
-  const planeGeometry = new PlaneGeometry(3, 3);
-  const planeMaterial = new MeshPhongMaterial({ color: 0x44aa88 });
-  const plane = new Mesh(planeGeometry, planeMaterial);
-  plane.position.y = 0;
-  plane.position.z = -1;
-  plane.rotation.x = 0;
-  scene.add(plane);
+  const groundGeometry = new PlaneGeometry(10, 10, 99, 99);
+  const groundMaterial = new MeshPhongMaterial({ color: 0x44aa88 });
+  // groundMaterial.bumpMap = generateGrassBumpMap(100, 100);
+  groundMaterial.displacementMap = generateGrassBumpMap(100, 100);
+  const ground = new Mesh(groundGeometry, groundMaterial);
+  ground.position.z = 0;
+  scene.add(ground);
+
+  const wallGeometry = new PlaneGeometry(6, 4, 99, 99);
+  const wallMaterial = new MeshStandardMaterial();
+  const bricksMap = generateBricksMap(100, 100, 255);
+  wallMaterial.displacementMap = bricksMap;
+  wallMaterial.displacementScale = 0.1;
+  wallMaterial.bumpMap = bricksMap;
+  wallMaterial.bumpScale = 5;
+  wallMaterial.map = generateBricksTexture(100, 100);
+  const wall = new Mesh(wallGeometry, wallMaterial);
+  wall.position.z = 2.5;
+  wall.position.y = 3;
+  wall.rotation.x = Math.PI / 2;
+  wall.rotation.z = Math.PI;
+  scene.add(wall);
 
   const light = new PointLight(0xffffff, 20);
   light.position.set(0, 0, 3);
@@ -139,4 +164,92 @@ function generateGradientMap() {
   const gradientMap = new DataTexture(data, size, 1, RedFormat);
   gradientMap.needsUpdate = true;
   return gradientMap;
+}
+
+function generateBumpMap(sizeX: number, sizeY: number, bumpFunction: (x: number, y: number, previousData: number[]) => number) {
+  const data = [];
+  for (let y = 0; y < sizeY; y++) {
+    for (let x = 0; x < sizeX; x++) {
+      const value = bumpFunction(x, y, data);
+      data.push(value);
+    }
+  }
+  const texture = new DataTexture(new Uint8Array(data), sizeX, sizeY, RedFormat);
+  texture.needsUpdate = true;
+  return texture;
+}
+
+function generateTextureMap(sizeX: number, sizeY: number, textureFunction: (x: number, y: number) => [number, number, number]) {
+  const data: number[] = [];
+  for (let y = 0; y < sizeY; y++) {
+    for (let x = 0; x < sizeX; x++) {
+      const value = textureFunction(x, y);
+      data.push(...value, 255);
+    }
+  }
+  const texture = new DataTexture(new Uint8Array(data), sizeX, sizeY);
+  texture.needsUpdate = true;
+  return texture;
+}
+
+function generateGrassBumpMap(sizeX: number, sizeY: number, grassHeight = 255) {
+  return generateBumpMap(sizeX, sizeY, (x, y, data) => {
+    const currentIndex = y * sizeX + x;
+    let wasTallGrassPastFewIndexes = false;
+    for (let i = 1; i < Math.random() * 20 + 20; i++) {
+      if (data[currentIndex - i] > grassHeight / 2) {
+        wasTallGrassPastFewIndexes = true;
+        break;
+      }
+    }
+    let wasMiddleGrassPastFewIndexes = false;
+    for (let i = 1; i < 6; i++) {
+      if (data[currentIndex - i] > grassHeight / 4) {
+        wasMiddleGrassPastFewIndexes = true;
+        break;
+      }
+    }
+    const wasTallGrassUp =
+      data[currentIndex - sizeX] > grassHeight / 2 || data[currentIndex - sizeX - 1] > grassHeight / 2 || data[currentIndex - sizeX + 1] > grassHeight / 2;
+    const wasMiddleGrassUp =
+      data[currentIndex - sizeX] > grassHeight / 4 || data[currentIndex - sizeX - 1] > grassHeight / 4 || data[currentIndex - sizeX + 1] > grassHeight / 4;
+    const wasTallGrass = wasTallGrassPastFewIndexes || wasTallGrassUp;
+    const wasMiddleGrass = wasMiddleGrassPastFewIndexes || wasMiddleGrassUp;
+    let grass = wasTallGrass ? (wasMiddleGrass ? 0 : grassHeight / 2) : grassHeight;
+    grass = (Math.random() / 2 + 0.5) * grass;
+    return grass;
+  });
+}
+
+function generateBricksMap(sizeX: number, sizeY: number, brickDepth = 255, brickWidth = 12, brickHeight = 7, mortarWidth = 1) {
+  return generateBumpMap(sizeX, sizeY, (x, y) => {
+    const mortarVertical = y % (brickHeight + mortarWidth) < mortarWidth;
+    const oddRow = Math.floor(y / (brickHeight + mortarWidth)) % 2 === 1;
+    const mortarHorizontal = (oddRow ? x + Math.floor(brickWidth / 2) : x) % (brickWidth + mortarWidth) < mortarWidth;
+    const brick = !mortarVertical && !mortarHorizontal;
+    const beginningOfBrick = brick && (oddRow ? x + Math.floor(brickWidth / 2) : x) % (brickWidth + mortarWidth) === 1;
+    const endingOfBrick = brick && (oddRow ? x + Math.floor(brickWidth / 2) : x) % (brickWidth + mortarWidth) === brickWidth;
+    const mortarLineOnTop = (y - 1) % (brickHeight + mortarWidth) === 0;
+    const mortarLineOnBottom = (y + 1) % (brickHeight + mortarWidth) === 0;
+    // slight chance of missing a corner of brick
+    if ((beginningOfBrick || endingOfBrick) && (mortarLineOnTop || mortarLineOnBottom) && Math.random() < 0.1) {
+      return brickDepth / 2;
+    }
+    return brick ? (Math.random() / 8 + 0.75) * brickDepth : 0;
+  });
+}
+
+function generateBricksTexture(sizeX: number, sizeY: number, brickWidth = 12, brickHeight = 7, mortarWidth = 1) {
+  return generateTextureMap(sizeX, sizeY, (x, y) => {
+    const mortarVertical = y % (brickHeight + mortarWidth) < mortarWidth;
+    const oddRow = Math.floor(y / (brickHeight + mortarWidth)) % 2 === 1;
+    const mortarHorizontal = (oddRow ? x + Math.floor(brickWidth / 2) : x) % (brickWidth + mortarWidth) < mortarWidth;
+    const brick = !mortarVertical && !mortarHorizontal;
+
+    if (brick) {
+      return [40, 10, 5];
+    } else {
+      return [50, 50, 50];
+    }
+  });
 }
